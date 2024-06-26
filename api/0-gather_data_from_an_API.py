464181ca20f1a85a32210bfadff69534a5abeb00
olaniyiolufemi <o.olufemi@alustudent.com>
2024-06-26 23:29:36 +0200

    niy3

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
-"""Script to get todos for a user from API"""
-
+"""Python Script that gather data from an API"""
 import requests
 import sys
 
-def main():
-    """main function"""
-    user_id = int(sys.argv[1])
-    todo_url = 'https://jsonplaceholder.typicode.com/todos'
-    user_url = 'https://jsonplaceholder.typicode.com/users'
 
-    response = requests.get(todo_url)
-    user_response = requests.get(user_url)
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    total_tasks = 0
-    completed_tasks = []
-    all_tasks = []
-    for todo in response.json():
-        if todo['userId'] == user_id:
-            total_tasks += 1
-            all_tasks.append(todo['title'])
-            if todo['completed']:
-                completed_tasks.append(todo['title'])
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    json_o = res.json()
+    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
 
-    users = user_response.json()
-    user_name = next((user['name'] for user in users if user['id'] == user_id), 'Unknown')
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            l_task.append(task)
 
-    print("Employee {} has completed {}/{} tasks:".format(user_name, len(completed_tasks), total_tasks))
-    for task in all_tasks:
-        print("\t {}".format(task))
-
-
-if __name__ == '__main__':
-    main()
+    print("({}/{}):".format(len(l_task), len(tasks)))
+    for task in l_task:
+        print("\t {}".format(task.get("title")))
